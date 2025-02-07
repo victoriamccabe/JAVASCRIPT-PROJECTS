1e17f8521492d6cc7d1c8ca01b250af323169fdb
victoriamccabe <victoriamccabe96@gmail.com>
2025-02-06 18:33:43 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+
+//Pop up Alert//
+var A="BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT";
+window.alert(A);
+
+//Variables//
+var Sent1="This is the beginning of the string", Sent2=" and this is the end of the string"
+
+//Expression//
+document.write(Sent1 + Sent2);
+
+
