7975bf1efb99048525083ea49384231145a09f2d
victoriamccabe <victoriamccabe96@gmail.com>
2025-02-08 14:10:18 -0700

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+
+//Function to display on click//
+function myFunction(){
+    var sentence= "I am so confused...";
+    sentence += " very confused.";
+    document.getElementById("test").innerHTML = sentence;
+}
