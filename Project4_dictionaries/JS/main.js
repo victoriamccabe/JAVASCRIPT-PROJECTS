7014442f9ed5388674de5886529b7804e50d85bd
victoriamccabe <victoriamccabe96@gmail.com>
2025-02-13 19:23:12 -0700

    PROJECT4_DICTIONARIES SUBMISSION ASSIGNMENT

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,12 @@
+function my_Dictionary(){
+    var Animal = {
+        Species:"Cat",
+        Color:"Orange",
+        Breed:"Domestic Short Hair",
+        Age:3,
+        Sound:"Meow!",
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML=Animal.Sound;
+}
+
